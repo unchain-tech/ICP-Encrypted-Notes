27f6af00b97d94204a32c5ea7bb315499d9901b4
Yuka Saito <yksaito02@gmail.com>
2022-12-23 22:20:48 +0900

    setup formatter

--- src/encrypted_notes_frontend/src/index.js
+++ src/encrypted_notes_frontend/src/index.js
@@ -1,19 +1,19 @@
-import { encrypted_notes_backend } from "../../declarations/encrypted_notes_backend";
+import { encrypted_notes_backend } from '../../declarations/encrypted_notes_backend';
 
-document.querySelector("form").addEventListener("submit", async (e) => {
+document.querySelector('form').addEventListener('submit', async (e) => {
   e.preventDefault();
-  const button = e.target.querySelector("button");
+  const button = e.target.querySelector('button');
 
-  const name = document.getElementById("name").value.toString();
+  const name = document.getElementById('name').value.toString();
 
-  button.setAttribute("disabled", true);
+  button.setAttribute('disabled', true);
 
   // Interact with foo actor, calling the greet method
   const greeting = await encrypted_notes_backend.greet(name);
 
-  button.removeAttribute("disabled");
+  button.removeAttribute('disabled');
 
-  document.getElementById("greeting").innerText = greeting;
+  document.getElementById('greeting').innerText = greeting;
 
   return false;
 });
