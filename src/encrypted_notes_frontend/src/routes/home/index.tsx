5b802b21d18771c74fe8ffe21646eb40da35c2a3
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-07-06 16:40:46 +0900

    feat: add Home page

--- src/encrypted_notes_frontend/src/routes/home/index.tsx
+++ src/encrypted_notes_frontend/src/routes/home/index.tsx
@@ -1,43 +1,30 @@
-import { Button, Input, InputGroup } from '@chakra-ui/react';
-import { useState } from 'react';
-
-import { encrypted_notes_backend } from '../../../../declarations/encrypted_notes_backend';
+import { Box, Button, Heading, Text } from '@chakra-ui/react';
 
 export const Home = () => {
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-
-  async function doGreet() {
-    const greeting = await encrypted_notes_backend.greet(name);
-    setMessage(greeting);
-  }
-
   return (
-    <div style={{ fontFamily: 'sans-serif' }}>
-      <div style={{ fontSize: '30px' }}>
-        <h1>Home</h1>
-        <p>Greetings, from DFINITY!</p>
-        <p>
-          {' '}
-          Type your message in the Name input field, then click{' '}
-          <b> Get Greeting</b> to display the result.
-        </p>
-      </div>
-      <div style={{ margin: '30px' }}>
-        <InputGroup>
-          <Input
-            id="name"
-            placeholder="Type text here"
-            value={name}
-            onChange={(ev) => setName(ev.target.value)}
-          />
-          <Button onClick={doGreet}>Get Greeting!</Button>
-        </InputGroup>
-      </div>
-      <div>
-        Greeting is: "<span style={{ color: 'green' }}>{message}</span>"
-      </div>
-    </div>
+    <Box
+      display={'flex'}
+      flexDirection={'column'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      minHeight={'calc(100vh - 64px)'}
+      px={{ base: '4', lg: '8' }}
+    >
+      <Heading
+        as={'h1'}
+        size={{ base: '2xl', lg: '4xl' }}
+        fontWeight={'bold'}
+        mb={'2rem'}
+      >
+        Encrypted Notes
+      </Heading>
+      <Text fontSize={'xl'} mb={'2rem'}>
+        Please authenticate with Internet Identity.
+      </Text>
+      <Button colorScheme={'green'} size={{ base: 'sm', lg: 'lg' }}>
+        Authenticate
+      </Button>
+    </Box>
   );
 };
 
