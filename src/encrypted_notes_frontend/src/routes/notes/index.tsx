083e6e83e62363a80b230a75ce69ac24bc57833f
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-07-11 22:07:25 +0900

    feat: add notes page

--- src/encrypted_notes_frontend/src/routes/notes/index.tsx
+++ src/encrypted_notes_frontend/src/routes/notes/index.tsx
@@ -1,13 +1,99 @@
-import Layout from '../../components/Layout';
+import { Box, Button, Flex, SimpleGrid, useDisclosure } from '@chakra-ui/react';
+import { useState } from 'react';
+import { FiPlus } from 'react-icons/fi';
+
+import { DeleteNoteDialog, Layout, NoteCard, NoteModal } from '../../components';
 
 export const Notes = () => {
+  const {
+    isOpen: isOpenNoteModal,
+    onOpen: onOpenNoteModal,
+    onClose: onCloseNoteModal,
+  } = useDisclosure();
+  const {
+    isOpen: isOpenDeleteDialog,
+    onOpen: onOpenDeleteDialog,
+    onClose: onCloseDeleteDialog,
+  } = useDisclosure();
+  const [mode, setMode] = useState<'add' | 'edit'>('add');
+  const [notes, setNotes] = useState<string[]>([]);
+  const [currentNote, setCurrentNote] = useState('');
+
+  const openAddNoteModal = () => {
+    setMode('add');
+    setCurrentNote('');
+    onOpenNoteModal();
+  };
+
+  const openEditNoteModal = (note: string) => {
+    setMode('edit');
+    setCurrentNote(note);
+    onOpenNoteModal();
+  };
+
+  const addNote = () => {
+    console.log('add note');
+    onCloseNoteModal();
+  };
+
+  const editNote = () => {
+    console.log('edit note');
+    onCloseNoteModal();
+  };
+
+  const deleteNote = () => {
+    console.log('delete note');
+    onCloseDeleteDialog();
+  };
+
   return (
-    <Layout>
-      <div>
-        <h1>Notes</h1>
-      </div>
-    </Layout>
+    <>
+      <Layout>
+        <Box p={6} overflowY={'auto'} maxHeight={'calc(100vh - 64px)'}>
+          <Flex mb={6} justifyContent={'flex-end'}>
+            <Button
+              leftIcon={<FiPlus />}
+              colorScheme={'green'}
+              variant={'outline'}
+              size={'lg'}
+              borderWidth={'4px'}
+              onClick={() => {
+                openAddNoteModal();
+              }}
+            >
+              New Note
+            </Button>
+          </Flex>
+          <SimpleGrid
+            spacing={4}
+            templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+          >
+            {notes.map((note, index) => (
+              <NoteCard
+                key={index}
+                note={note}
+                handleOpenDeleteDialog={onOpenDeleteDialog}
+                handleOpenEditModal={openEditNoteModal}
+              />
+            ))}
+          </SimpleGrid>
+        </Box>
+      </Layout>
+
+      <NoteModal
+        isOpen={isOpenNoteModal}
+        onClose={onCloseNoteModal}
+        title={mode === 'add' ? 'Add note' : 'Edit note'}
+        currentNote={currentNote}
+        setCurrentNote={setCurrentNote}
+        handleSaveNote={mode === 'add' ? addNote : editNote}
+      />
+
+      <DeleteNoteDialog
+        isOpen={isOpenDeleteDialog}
+        onClose={onCloseDeleteDialog}
+        handleDeleteNote={deleteNote}
+      />
+    </>
   );
 };
-
-export default Notes;
