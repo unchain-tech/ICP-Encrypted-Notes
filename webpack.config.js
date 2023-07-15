4956b6e0e1380d84912572b102d53a3ee4798d03
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-07-15 14:38:21 +0900

    chore: formatting with prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,45 @@
-require("dotenv").config();
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+require('dotenv').config();
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
-const isDevelopment = process.env.NODE_ENV !== "production";
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
-const frontendDirectory = "encrypted_notes_frontend";
+const frontendDirectory = 'encrypted_notes_frontend';
 
-const frontend_entry = path.join("src", frontendDirectory, "src", "index.html");
+const frontend_entry = path.join('src', frontendDirectory, 'src', 'index.html');
 
 module.exports = {
-  target: "web",
-  mode: isDevelopment ? "development" : "production",
+  target: 'web',
+  mode: isDevelopment ? 'development' : 'production',
   entry: {
     // The frontend.entrypoint points to the HTML file for this build, so we need
     // to replace the extension to `.js`.
-    index: path.join(__dirname, frontend_entry).replace(/\.html$/, ".tsx"),
+    index: path.join(__dirname, frontend_entry).replace(/\.html$/, '.tsx'),
   },
-  devtool: isDevelopment ? "source-map" : false,
+  devtool: isDevelopment ? 'source-map' : false,
   optimization: {
     minimize: !isDevelopment,
     minimizer: [new TerserPlugin()],
   },
   resolve: {
-    extensions: [".js", ".ts", ".jsx", ".tsx"],
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
     fallback: {
-      assert: require.resolve("assert/"),
-      buffer: require.resolve("buffer/"),
-      events: require.resolve("events/"),
-      stream: require.resolve("stream-browserify/"),
-      util: require.resolve("util/"),
+      assert: require.resolve('assert/'),
+      buffer: require.resolve('buffer/'),
+      events: require.resolve('events/'),
+      stream: require.resolve('stream-browserify/'),
+      util: require.resolve('util/'),
     },
   },
   output: {
-    filename: "index.js",
-    path: path.join(__dirname, "dist", frontendDirectory),
+    filename: 'index.js',
+    path: path.join(__dirname, 'dist', frontendDirectory),
   },
   module: {
-    rules: [
-      { test: /\.(js|ts)x?$/, loader: "ts-loader" }
-    ]
+    rules: [{ test: /\.(js|ts)x?$/, loader: 'ts-loader' }],
   },
   // Depending in the language or framework you are using for
   // front-end development, add module loaders to the default
@@ -61,20 +59,20 @@ module.exports = {
     }),
     new webpack.EnvironmentPlugin([
       ...Object.keys(process.env).filter((key) => {
-        if (key.includes("CANISTER")) return true;
-        if (key.includes("DFX")) return true;
+        if (key.includes('CANISTER')) return true;
+        if (key.includes('DFX')) return true;
         return false;
       }),
     ]),
     new webpack.ProvidePlugin({
-      Buffer: [require.resolve("buffer/"), "Buffer"],
-      process: require.resolve("process/browser"),
+      Buffer: [require.resolve('buffer/'), 'Buffer'],
+      process: require.resolve('process/browser'),
     }),
     new CopyPlugin({
       patterns: [
         {
           from: `src/${frontendDirectory}/src/.ic-assets.json*`,
-          to: ".ic-assets.json5",
+          to: '.ic-assets.json5',
           noErrorOnMissing: true,
         },
       ],
@@ -84,17 +82,17 @@ module.exports = {
   // if you edit dfx.json to define a project-specific local network, change the port to match.
   devServer: {
     proxy: {
-      "/api": {
-        target: "http://127.0.0.1:4943",
+      '/api': {
+        target: 'http://127.0.0.1:4943',
         changeOrigin: true,
         pathRewrite: {
-          "^/api": "/api",
+          '^/api': '/api',
         },
       },
     },
-    static: path.resolve(__dirname, "src", frontendDirectory, "assets"),
+    static: path.resolve(__dirname, 'src', frontendDirectory, 'assets'),
     hot: true,
-    watchFiles: [path.resolve(__dirname, "src", frontendDirectory)],
+    watchFiles: [path.resolve(__dirname, 'src', frontendDirectory)],
     liveReload: true,
     historyApiFallback: true,
   },
