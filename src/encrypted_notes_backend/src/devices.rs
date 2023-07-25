b05281d2f53554e07b003404b3fd8de8f306e9d5
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-07-25 22:28:38 +0900

    feat: add register, get and unregister functions for device

---
+++ src/encrypted_notes_backend/src/devices.rs
@@ -0,0 +1,75 @@
+use candid::CandidType;
+use ic_cdk::export::Principal;
+use serde::{Deserialize, Serialize};
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+
+pub type DeviceAlias = String;
+pub type PublicKey = String;
+pub type EncryptedSymmetricKey = String;
+
+pub type RegisterDeviceResult = Result<(), String>;
+
+#[derive(CandidType, Clone, Serialize, Deserialize)]
+pub struct DeviceData {
+    pub aliases: HashMap<DeviceAlias, PublicKey>,
+    pub keys: HashMap<PublicKey, EncryptedSymmetricKey>,
+}
+
+#[derive(Default)]
+pub struct Devices {
+    pub devices: HashMap<Principal, DeviceData>,
+}
+
+impl Devices {
+    pub fn register_device(
+        &mut self,
+        caller: Principal,
+        alias: DeviceAlias,
+        public_key: PublicKey,
+    ) -> RegisterDeviceResult {
+        match self.devices.entry(caller) {
+            // 既にPrincipalが登録されている場合は、デバイスデータを追加します。
+            Entry::Occupied(mut device_data_entry) => {
+                let device_data = device_data_entry.get_mut();
+                match device_data.aliases.entry(alias) {
+                    Entry::Occupied(_) => Err("Device alias is already registered".to_string()),
+                    Entry::Vacant(alias_entry) => {
+                        alias_entry.insert(public_key);
+                        Ok(())
+                    }
+                }
+            }
+            // 初めて登録する場合は、Principalとデバイスデータを紐づけて保存します。
+            Entry::Vacant(empty_device_data_entry) => {
+                let mut device_data = DeviceData {
+                    aliases: HashMap::new(),
+                    keys: HashMap::new(),
+                };
+                // デバイスエイリアスと公開鍵を紐づけて保存します。
+                device_data.aliases.insert(alias, public_key);
+                empty_device_data_entry.insert(device_data);
+                Ok(())
+            }
+        }
+    }
+
+    pub fn get_device_aliases(&self, caller: Principal) -> Vec<DeviceAlias> {
+        self.devices
+            .get(&caller)
+            .map(|device_data| device_data.aliases.keys().cloned().collect())
+            .unwrap_or_default()
+    }
+
+    pub fn unregister_device(&mut self, caller: Principal, alias: DeviceAlias) {
+        if let Some(device_data) = self.devices.get_mut(&caller) {
+            // Principalは、必ず1つ以上のデバイスエイリアスが紐づいているものとします。
+            assert!(device_data.aliases.len() > 1);
+
+            let public_key = device_data.aliases.remove(&alias);
+            if let Some(public_key) = public_key {
+                device_data.keys.remove(&public_key);
+            }
+        }
+    }
+}
--- src/encrypted_notes_backend/src/lib.rs
+++ src/encrypted_notes_backend/src/lib.rs
@@ -1,12 +1,15 @@
+use crate::devices::*;
 use crate::notes::*;
 use ic_cdk::api::caller as caller_api;
 use ic_cdk::export::Principal;
 use ic_cdk_macros::*;
 use std::cell::RefCell;
 
+mod devices;
 mod notes;
 
 thread_local! {
+    static DEVICES: RefCell<Devices> = RefCell::default();
     static NOTES: RefCell<Notes> = RefCell::default();
 }
 
@@ -21,15 +24,52 @@ fn caller() -> Principal {
     caller
 }
 
+fn is_caller_registered(caller: Principal) -> bool {
+    DEVICES.with(|devices| devices.borrow().devices.contains_key(&caller))
+}
+
+#[update(name = "registerDevice")]
+fn register_device(alias: DeviceAlias, public_key: PublicKey) -> RegisterDeviceResult {
+    let caller = caller();
+
+    DEVICES.with(|devices| {
+        devices
+            .borrow_mut()
+            .register_device(caller, alias, public_key)
+    })
+}
+
+#[query(name = "getDeviceAliases")]
+fn get_device_aliases() -> Vec<DeviceAlias> {
+    let caller = caller();
+    assert!(is_caller_registered(caller));
+
+    DEVICES.with(|devices| devices.borrow().get_device_aliases(caller))
+}
+
+#[update(name = "unregisterDevice")]
+fn unregister_device(alias: DeviceAlias) {
+    let caller = caller();
+    assert!(is_caller_registered(caller));
+
+    DEVICES.with(|devices| {
+        devices.borrow_mut().unregister_device(caller, alias);
+    })
+}
+
 #[query(name = "getNotes")]
 fn get_notes() -> Vec<EncryptedNote> {
     let caller = caller();
+    assert!(is_caller_registered(caller));
+
     NOTES.with(|notes| notes.borrow().get_notes(caller))
 }
 
 #[update(name = "addNote")]
 fn add_note(encrypted_text: String) {
     let caller = caller();
+    assert!(is_caller_registered(caller));
+
     NOTES.with(|notes| {
         notes.borrow_mut().add_note(caller, encrypted_text);
     })
@@ -38,6 +78,8 @@ fn add_note(encrypted_text: String) {
 #[update(name = "deleteNote")]
 fn delete_note(id: u128) {
     let caller = caller();
+    assert!(is_caller_registered(caller));
+
     NOTES.with(|notes| {
         notes.borrow_mut().delete_note(caller, id);
     })
@@ -46,6 +88,8 @@ fn delete_note(id: u128) {
 #[update(name = "updateNote")]
 fn update_note(new_note: EncryptedNote) {
     let caller = caller();
+    assert!(is_caller_registered(caller));
+
     NOTES.with(|notes| {
         notes.borrow_mut().update_note(caller, new_note);
     })
