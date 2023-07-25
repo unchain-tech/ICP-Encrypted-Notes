c958dcf1a195ceda365ffa6c53d0424e6b2137ba
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-07-25 22:29:21 +0900

    chore: add device test

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,6 +16,10 @@ compare_result() {
 }
 
 TEST_STATUS=0
+TEST_DEVICE_ALIAS_01='TEST_DEVICE_ALIAS_01'
+TEST_DEVICE_ALIAS_02='TEST_DEVICE_ALIAS_02'
+TEST_PUBLIC_KEY_01='TEST_PUBLIC_KEY_01'
+TEST_PUBLIC_KEY_02='TEST_PUBLIC_KEY_02'
 
 # ===== 準備 =====
 dfx stop
@@ -30,17 +34,48 @@ dfx identity use test-user
 dfx deploy encrypted_notes_backend
 
 # ===== テスト =====
+FUNCTION='registerDevice'
+echo "===== $FUNCTION ====="
+EXPECT='(variant { Ok })'
+RESULT=`dfx canister call encrypted_notes_backend $FUNCTION '('\"$TEST_DEVICE_ALIAS_01\"', '\"$TEST_PUBLIC_KEY_01\"')'`
+compare_result "Return Ok" "$EXPECT" "$RESULT" || TEST_STATUS=1
+
+EXPECT='(variant { Err = "Device alias is already registered" })'
+RESULT=`dfx canister call encrypted_notes_backend $FUNCTION '('\"$TEST_DEVICE_ALIAS_01\"', '\"$TEST_PUBLIC_KEY_01\"')'`
+compare_result "Error duplicate device alias" "$EXPECT" "$RESULT" || TEST_STATUS=1
+
+
+FUNCTION='getDeviceAliases'
+echo "===== $FUNCTION ====="
+EXPECT='(vec { '\"$TEST_DEVICE_ALIAS_01\"' })'
+RESULT=`dfx canister call encrypted_notes_backend $FUNCTION`
+compare_result "Return device list" "$EXPECT" "$RESULT" || TEST_STATUS=1
+
+
+FUNCTION='unregisterDevice'
+# 別のデバイスを登録する
+UNUSED_RESULR=`dfx canister call encrypted_notes_backend registerDevice '('\"$TEST_DEVICE_ALIAS_02\"', '\"$TEST_PUBLIC_KEY_02\"')'`
+echo "===== $FUNCTION ====="
+EXPECT='()'
+RESULT=`dfx canister call encrypted_notes_backend $FUNCTION '('\"$TEST_DEVICE_ALIAS_01\"')'`
+compare_result "Return none" "$EXPECT" "$RESULT" || TEST_STATUS=1
+# 確認
+FUNCTION='getDeviceAliases'
+EXPECT='(vec { '\"$TEST_DEVICE_ALIAS_02\"' })'
+RESULT=`dfx canister call encrypted_notes_backend $FUNCTION`
+compare_result "Check with $FUNCTION" "$EXPECT" "$RESULT" || TEST_STATUS=1
+
 FUNCTION='addNote'
 echo "===== $FUNCTION ====="
 EXPECT='()'
 RESULT=`dfx canister call encrypted_notes_backend $FUNCTION '("First text!")'`
-compare_result "$FUNCTION" "$EXPECT" "$RESULT" || TEST_STATUS=1
+compare_result "Return none" "$EXPECT" "$RESULT" || TEST_STATUS=1
 
 FUNCTION='getNotes'
 echo "===== $FUNCTION ====="
 EXPECT='(vec { record { id = 0 : nat; encrypted_text = "First text!" } })'
 RESULT=`dfx canister call encrypted_notes_backend $FUNCTION`
-compare_result "$FUNCTION" "$EXPECT" "$RESULT" || TEST_STATUS=1
+compare_result "Return note list" "$EXPECT" "$RESULT" || TEST_STATUS=1
 
 FUNCTION='updateNote'
 echo "===== $FUNCTION ====="
@@ -51,23 +86,23 @@ RESULT=`dfx canister call encrypted_notes_backend $FUNCTION '(
     encrypted_text = "Updated first text!"
   }
 )'`
-compare_result "$FUNCTION" "$EXPECT" "$RESULT" || TEST_STATUS=1
+compare_result "Return none" "$EXPECT" "$RESULT" || TEST_STATUS=1
 # 確認
 FUNCTION='getNotes'
 EXPECT='(vec { record { id = 0 : nat; encrypted_text = "Updated first text!" } })'
 RESULT=`dfx canister call encrypted_notes_backend $FUNCTION`
-compare_result "$FUNCTION" "$EXPECT" "$RESULT" || TEST_STATUS=1
+compare_result "Check with $FUNCTION" "$EXPECT" "$RESULT" || TEST_STATUS=1
 
 FUNCTION='deleteNote'
 echo "===== $FUNCTION ====="
 EXPECT='()'
 RESULT=`dfx canister call encrypted_notes_backend $FUNCTION '(0)'`
-compare_result "$FUNCTION" "$EXPECT" "$RESULT" || TEST_STATUS=1
+compare_result "Return none" "$EXPECT" "$RESULT" || TEST_STATUS=1
 # 確認
 FUNCTION='getNotes'
 EXPECT='(vec {})'
 RESULT=`dfx canister call encrypted_notes_backend $FUNCTION`
-compare_result "$FUNCTION" "$EXPECT" "$RESULT" || TEST_STATUS=1
+compare_result "Check with $FUNCTION" "$EXPECT" "$RESULT" || TEST_STATUS=1
 
 # ===== 後始末 =====
 dfx identity use default
